f26a5370a12a50f174960cbc6107299dfe2680d2
seyiojo <seyi.ojo@mindgeekconsultants.com>
2022-08-27 15:53:00 +0100

    Modification on Subscriber

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Notification;
 
 class NotificationController extends Controller
 {
     public function topicNotification(Request $request)
     {
-        $json = file_get_contents('php://input');
-        $data = json_decode($json);
+        //$request->data sent from POSTMAN should bejson_encode($request->data)
+        $response['topic'] = $request->topic;
+        $response['data'] = $request->data; // if coming from POSTMAN should be json_encode($request->data)
+
+        
+        //Uncomment this block to save notification in DB...make sure the DB is set to the Publisher DB
+        $notification = new Notification();
+        $notification->topic = $response['topic'];
+        $notification->message = $response['data'];
+        $notification->save();
+        
+
+        return response($response, 200);
     }
 }
